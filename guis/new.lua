a167d6c1f2f8c2ce515e85453404e5634ee1cd14
Loqrp <loqrpreal@gmail.com>
2025-08-17 13:53:27 +0300

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -5931,7 +5931,7 @@ general:CreateButton({
 	Function = function()
 		shared.vapereload = true
 		if shared.VapeDeveloper then
-			loadstring(readfile('vectorclient/loader.lua'), 'loader')()
+			loadstring(readfile('vectorclient/main.lua'), 'loader')()
 		else
 			loadstring(game:HttpGet('https://raw.githubusercontent.com/Loqrp/vectorclient/'..readfile('vectorclient/profiles/commit.txt')..'/loader.lua', true))()
 		end
