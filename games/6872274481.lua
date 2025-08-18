32cc15534a00c8db2abaa1b263e07d10376e0ffd
Loqrp <loqrpreal@gmail.com>
2025-08-18 19:16:32 +0300

    b

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -2299,6 +2299,9 @@ run(function()
 				end
 			end
 		end,
+		ExtraText = function()
+			return math.floor(ChargeTime.Value)
+		end,
 		Tooltip = 'Attack players around you\nwithout aiming at them.'
 	})
 	Targets = Killaura:CreateTargets({
