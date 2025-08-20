53950d1b5a186d65ceda9bb688b480f85f291350
Loqrp <loqrpreal@gmail.com>
2025-08-20 16:40:22 +0300

    i LOVE skidding

--- guis/new.lua
+++ guis/new.lua
@@ -6031,6 +6031,7 @@ scaleslider = guipane:CreateSlider({
 	Darker = true,
 	Visible = false
 })
+
 guipane:CreateDropdown({
 	Name = 'GUI Theme',
 	List = inputService.TouchEnabled and {'new', 'old'} or {'new', 'old', 'rise'},
@@ -6453,6 +6454,7 @@ VapeLabelSorter.Parent = VapeLabelHolder
 local targetinfo
 local targetinfoobj
 local targetinfobcolor
+local targetinfofollow
 targetinfoobj = mainapi:CreateOverlay({
 	Name = 'Target Info',
 	Icon = getcustomasset('vectorclient/assets/new/targetinfoicon.png'),
@@ -6464,6 +6466,12 @@ targetinfoobj = mainapi:CreateOverlay({
 			task.spawn(function()
 				repeat
 					targetinfo:UpdateInfo()
+					if targetinfofollow and targetinfofollow.Enabled and targetinfo:UpdateInfo() then
+						local vec, screen = workspace.CurrentCamera:WorldToScreenPoint(target.Position)
+						if screen then
+							targetinfobkg.Parent.Parent.Position = UDim2.fromOffset(vec.X, vec.Y)
+						end
+					end
 					task.wait()
 				until not targetinfoobj.Button or not targetinfoobj.Button.Enabled
 			end)
@@ -6569,6 +6577,11 @@ local targetinfobackgroundtransparency = {
 }
 local targetinfodisplay = targetinfoobj:CreateToggle({
 	Name = 'Use Displayname',
+	Default = true
+})
+targetinfofollow = targetinfoobj:CreateToggle({
+	Name = 'Follow Player',
+	Function = function(callback) end,
 	Default = true
 })
 targetinfoobj:CreateToggle({
